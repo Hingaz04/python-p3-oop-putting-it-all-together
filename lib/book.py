642bfb69ed5414fc2c4e2cb9b4edd9347362e20a
Hingaz <hhinga04@gmail.com>
2023-12-08 15:57:38 +0300

    Completed Lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
+
 class Book:
-    pass
-    
-        
+
+    def test_has_title_and_page_count(self):
+        '''has the title and page_count passed into __init__, and values can be set to a new instance.'''
+        book = Book(title="And Then There Were None",
+                    page_count=272)
+        assert isinstance(book, Book)
